08808e0cd15e210cd5068c84ad244b1ad13af142
Aaron Kable <aaronkable@gmail.com>
2022-01-04 12:55:44 +0800

    update files

--- api_write.php
+++ api_write.php
@@ -1,12 +1,18 @@
 <?php
-error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
     $api_key=include('api_config.php');
+    file_put_contents('./api.log', $api_key."\n", FILE_APPEND);
+    file_put_contents('./api.log', $_POST['x-api-key']."\n", FILE_APPEND);
     if ($_POST['x-api-key']==$api_key){
         $file_name="pathfinder-auth.ini";
         $folder="config/";
         $file_name=$folder."".$file_name;
+        file_put_contents('./api.log', $_POST['character_ids']."\n", FILE_APPEND);
         $output = shell_exec('/var/www/html/pathfinder/app/update_characters.sh '.$_POST['character_ids']);
-        return $output
+        file_put_contents('./api.log', $output."\n", FILE_APPEND);
+        file_put_contents('./api.log', "DONE!"."\n", FILE_APPEND);
+        return $output;
     }
-/>
-
+?>
--- update_characters.sh
+++ update_characters.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+echo $1
 function replace_setting() {
-    sed -i -E "s/$1/$2/g" $3
+    mkdir /var/www/html/pathfinder/conf/
+    sed -E "s/$1/$2/g" $3 > /var/www/html/pathfinder/conf/pathfinder.ini;
 }
 echo "Replacing settings!"
-replace_setting "CHARACTER\s*=\s*.*" "CHARACTER          =   $1" "/var/www/pathfinder/app/pathfinder.ini"
+replace_setting "CHARACTER\s*=\s*.*" "CHARACTER                     =   $1" "/var/www/html/pathfinder/app/pathfinder.ini"
 echo "Complete!"
